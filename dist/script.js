180cf45f8330b04944433899afa89e36f98064c2
Evgenii Borisovich <anarkilimitz@gmail.com>
2025-11-18 21:05:35 +0500

    add-js-forms-calc-price

--- dist/script.js
+++ dist/script.js
@@ -13824,25 +13824,36 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 // модалка
 const modals = () => {
-  function bindModal(triggerSelector, modalSelector, closeSelector) {
+  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
-      close = document.querySelector(closeSelector);
+      close = document.querySelector(closeSelector),
+      windows = document.querySelectorAll('[data-modal]'); // закрыть все модалки
+
     trigger.forEach(item => {
       item.addEventListener('click', e => {
         if (e.target) {
           e.preventDefault();
         }
+        windows.forEach(item => {
+          item.style.display = 'none'; // закрыть все модалки
+        });
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
       });
     });
     close.addEventListener('click', () => {
+      windows.forEach(item => {
+        item.style.display = 'none'; // закрыть все модалки
+      });
       modal.style.display = 'none';
       document.body.style.overflow = '';
     });
     modal.addEventListener('click', e => {
-      if (e.target === modal) {
+      if (e.target === modal && closeClickOverlay) {
+        windows.forEach(item => {
+          item.style.display = 'none'; // закрыть все модалки
+        });
         modal.style.display = 'none';
         document.body.style.overflow = '';
       }
@@ -13857,8 +13868,14 @@ const modals = () => {
 
   // это ВЫЗВАТЬ ЗАМЕРЩИКА
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
-  bindModal('.phone_link', '.popup', '.popup .popup_close'); // это ссылки
-
+  // это МОДАЛКА ПО ссылке
+  bindModal('.phone_link', '.popup', '.popup .popup_close');
+  // это РАССЧИТАТЬ СТОИМОСТЬ
+  bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+  // это РАССЧИТАТЬ СТОИМОСТЬ - вторая МОДАЛКА
+  bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+  // это РАССЧИТАТЬ СТОИМОСТЬ - третья МОДАЛКА
+  bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
   // showModalByTime('.popup', 60000);
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (modals);
@@ -13876,7 +13893,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass, display = 'block') => {
   const header = document.querySelector(headerSelector),
     tab = document.querySelectorAll(tabSelector),
     content = document.querySelectorAll(contentSelector);
@@ -13889,7 +13906,7 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
     });
   }
   function showTabContent(i = 0) {
-    content[i].style.display = 'block';
+    content[i].style.display = display;
     tab[i].classList.add(activeClass);
   }
   hideTabContent();
@@ -14087,6 +14104,7 @@ window.addEventListener('DOMContentLoaded', () => {
   (0,_modules_modals__WEBPACK_IMPORTED_MODULE_1__["default"])();
   (0,_modules_tabs__WEBPACK_IMPORTED_MODULE_2__["default"])('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
   (0,_modules_tabs__WEBPACK_IMPORTED_MODULE_2__["default"])('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+  (0,_modules_tabs__WEBPACK_IMPORTED_MODULE_2__["default"])('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
   (0,_modules_forms__WEBPACK_IMPORTED_MODULE_3__["default"])();
 });
 })();
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,9 +1,15 @@
 // модалка
 const modals = () => {
-	function bindModal(triggerSelector, modalSelector, closeSelector) {
+	function bindModal(
+		triggerSelector,
+		modalSelector,
+		closeSelector,
+		closeClickOverlay = true
+	) {
 		const trigger = document.querySelectorAll(triggerSelector),
 			modal = document.querySelector(modalSelector),
-			close = document.querySelector(closeSelector);
+			close = document.querySelector(closeSelector),
+			windows = document.querySelectorAll('[data-modal]'); // закрыть все модалки
 
 		trigger.forEach((item) => {
 			item.addEventListener('click', (e) => {
@@ -11,18 +17,30 @@ const modals = () => {
 					e.preventDefault();
 				}
 
+				windows.forEach((item) => {
+					item.style.display = 'none'; // закрыть все модалки
+				});
+
 				modal.style.display = 'block';
 				document.body.style.overflow = 'hidden';
 			});
 		});
 
 		close.addEventListener('click', () => {
+			windows.forEach((item) => {
+				item.style.display = 'none'; // закрыть все модалки
+			});
+
 			modal.style.display = 'none';
 			document.body.style.overflow = '';
 		});
 
 		modal.addEventListener('click', (e) => {
-			if (e.target === modal) {
+			if (e.target === modal && closeClickOverlay) {
+				windows.forEach((item) => {
+					item.style.display = 'none'; // закрыть все модалки
+				});
+
 				modal.style.display = 'none';
 				document.body.style.overflow = '';
 			}
@@ -42,9 +60,24 @@ const modals = () => {
 		'.popup_engineer',
 		'.popup_engineer .popup_close'
 	);
-
-	bindModal('.phone_link', '.popup', '.popup .popup_close'); // это ссылки
-
+	// это МОДАЛКА ПО ссылке
+	bindModal('.phone_link', '.popup', '.popup .popup_close');
+	// это РАССЧИТАТЬ СТОИМОСТЬ
+	bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+	// это РАССЧИТАТЬ СТОИМОСТЬ - вторая МОДАЛКА
+	bindModal(
+		'.popup_calc_button',
+		'.popup_calc_profile',
+		'.popup_calc_profile_close',
+		false
+	);
+	// это РАССЧИТАТЬ СТОИМОСТЬ - третья МОДАЛКА
+	bindModal(
+		'.popup_calc_profile_button',
+		'.popup_calc_end',
+		'.popup_calc_end_close',
+		false
+	);
 	// showModalByTime('.popup', 60000);
 };
 
